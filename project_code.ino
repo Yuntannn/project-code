fc59b1df469878888869d914f561b0f567a63a2f
Yuntannn <48405672+Yuntannn@users.noreply.github.com>
2019-05-24 13:15:26 +1000

    Add files via upload

---
+++ project_code.ino
@@ -0,0 +1,38 @@
+/* Student name: Yuntan SUN
+ *  Student ID: 218011232
+ *  Description:  use the multiple sensors
+ */
+
+
+const int TrigPin = 12;
+const int EchoPin = 11;
+
+float cm;
+ void setup(){
+  pinMode(TrigPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
+
+  Serial.begin(9600);
+ }
+
+ void loop(){
+  digitalWrite(TrigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TrigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TrigPin, LOW);
+
+  cm = pulseIn(EchoPin, HIGH) / 58.0;
+  cm = (int(cm * 100.0)) / 100.0;
+  Serial.print(" The current distence is: ");
+  Serial.print(cm);
+  Serial.print("cm. ");
+  Serial.println();
+  delay(1000);
+  if (cm < 100){
+    Serial.print("WARNING!!! TOO CLOSE");
+  }
+ }
+
+
+ 
